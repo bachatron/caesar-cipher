51ce51bd0fa9e38578d79fcdf3d66e2a755fda49
IAIR <idz420@gmail.com>
2023-07-04 14:59:43 -0300

    Added some rspec test examples

--- spec/ccipher_spec.rb
+++ spec/ccipher_spec.rb
@@ -7,4 +7,19 @@ describe Ccipher do
     expect(ccipher.play('hello', 1)).to eql('ifmmp')
   end
 
+  it 'takes more than 1 word' do
+    ccipher = Ccipher.new
+    expect(ccipher.play('hello world', 1)).to eql('ifmmp xpsme')
+  end
+
+  it 'can cipher upcase and downcase words' do
+    ccipher = Ccipher.new
+    expect(ccipher.play('HeLLo wOrLd', 1)).to eql('IfMMp xPsMe')
+  end
+
+  it 'takes more than 26 shift value' do
+    ccipher = Ccipher.new
+    expect(ccipher.play('Hello world!', 34)).to eql('Pmttw ewztl!')
+  end
+
 end
